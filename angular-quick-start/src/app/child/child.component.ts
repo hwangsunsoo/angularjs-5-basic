676d352c901f35707ba2c7fa09fe1924a330b2b9
we <we@2018060030.local>
2018-08-14 18:58:15 +0900

    .

--- angular-quick-start/src/app/child/child.component.ts
+++ angular-quick-start/src/app/child/child.component.ts
@@ -1,22 +1,45 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterContentInit, AfterViewInit, AfterContentChecked, AfterViewChecked, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit, AfterContentChecked, AfterViewChecked {
   @Input() name;
   @Input() age;
 
   @Output() next = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+    console.log('constructor');
+  }
+
+  ngOnChanges() {
+    console.error('ngOnChanges');
+  }
 
   ngOnInit() {
+    console.log('ngOnInit');
     setInterval(() => {
       this.next.emit();
     }, 1000);
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
   }
 
   click(param1: HTMLInputElement, param2: string, param3: string) {
