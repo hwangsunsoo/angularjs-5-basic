fd168d83c76df6e4c320f2522015393f57580509
we <we@2018060030.local>
2018-08-20 18:45:56 +0900

    부트스트랩 데모

---
+++ angular-demo-basic1/src/app/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: Http) { }
+
+  public getUsers(): Promise<any> {
+    return this.http.get('https://raw.githubusercontent.com/2woongjae/angular-demo-basic1/master/data.json')
+      .toPromise();
+  }
+
+  public getUsersObservable() {
+    return this.http.get('https://raw.githubusercontent.com/2woongjae/angular-demo-basic1/master/data.json');
+  }
+
+}
