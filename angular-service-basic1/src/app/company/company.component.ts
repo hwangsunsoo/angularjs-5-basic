c7e2239b1bb2e620269493bbb4794ad01f700fab
we <we@2018060030.local>
2018-08-20 16:44:37 +0900

    서비스

---
+++ angular-service-basic1/src/app/company/company.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { LogService } from '../log.service';
+
+@Component({
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
+})
+export class CompanyComponent implements OnInit {
+
+  constructor(private logService: LogService) {
+    this.logService.info('Company');
+  }
+
+  ngOnInit() {
+  }
+
+}
